8a330110a1fd35cbed95582d3caca54bf7841039
Akshay Devrale <akshaydevrale@gmail.com>
2025-08-30 16:16:35 +0530

    Update one.js

--- javascript/04_control_flow/one.js
+++ javascript/04_control_flow/one.js
@@ -1,27 +1,32 @@
-// if
+//-------------------- if statements & comparison operators --------------------
 
-// <, >, <=, >=, ==, !=, ===
+// Operators: <, >, <=, >=, ==, !=, ===
 
 const isUserLoggedIn = true
 const temperature = 41
 
 if (temperature < 50){
-    console.log("less than 50");
+    console.log("less than 50");  // => 41 < 50 true, so this block runs
 } else {
-    console.log("greater than 50");
+    console.log("greater than 50");  // => Will not run in this case
 }
 
 
 const score = 200
 if(score > 100) {
     const power = "fly"
-    console.log(`User power: ${power}`);
+    console.log(`User power: ${power}`);  // => prints User power: fly
 }
 //console.log(`User power: ${power}`);
+// => Error: 'power' is block-scoped, cannot be accessed outside
 
+
+//-------------------- single-line if --------------------
 
 const balance = 1000
-if(balance > 500) console.log("test");
+if(balance > 500) console.log("test");  // => If only one statement,{} can be skipped prints: test
+
+//-------------------- else if --------------------
 
 if(balance < 500){
     console.log("less than 500");
@@ -31,17 +36,27 @@ if(balance < 500){
     console.log("less than 900");
 } else {
     console.log("less than 1200");
+    // => 1000 < 1200 is true prints this
 }
 
+
+//-------------------- Logical AND (&&) --------------------
 
 const userLoggedIn = true
 const debitCard = true
 if(userLoggedIn && debitCard) {
     console.log("Allow to buy course");
+    // => && means all conditions must be true
+    // => true && true prints: Allow to buy course
 }
+
+
+//-------------------- Logical OR (||) --------------------
 
 const loggedInFromGoogle = false
 const loggedInFromEmail = true
 if(loggedInFromGoogle || loggedInFromEmail){
     console.log("User logged in");
+    // => || means at least onw condition must be true
+    // => false || true prints: User logged in
 }
